3b3dc472f7a6c952327809be494703dfecf60522
Karen Nemzek <kcnemzek@gmail.com>
2026-03-24 14:29:36 -0400

    Fix issue with copy link on iOS

--- src/components/layout/AppMenuSheet.tsx
+++ src/components/layout/AppMenuSheet.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ChefHat, Link as LinkIcon, Users, Plus, X, RefreshCw } from "lucide-react";
 import BottomSheet from "@/components/shared/BottomSheet";
 import { useAppContext } from "@/store/context";

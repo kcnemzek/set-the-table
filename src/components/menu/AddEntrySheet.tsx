f04cfaebbc73ad3cd8d25273466efd947e6cfb10
Karen Nemzek <kcnemzek@gmail.com>
2026-04-13 15:28:12 -0400

    Enhancement: User Experience Improvements around custom recipes.

--- src/components/menu/AddEntrySheet.tsx
+++ src/components/menu/AddEntrySheet.tsx
@@ -300,7 +300,6 @@ export default function AddEntrySheet({
       open={!!viewingCustomRecipe}
       onClose={() => setViewingCustomRecipe(null)}
       existing={viewingCustomRecipe ?? undefined}
-      readOnly
     />
 
     <RecipeDetailSheet

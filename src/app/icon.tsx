e1dfd83e9c4b4f6127880e7c8256292c00897524
Karen Nemzek <kcnemzek@gmail.com>
2026-04-19 14:27:01 -0400

    Bug: Icon isn't showing properly in browser tab.

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -2,29 +2,28 @@ import { ImageResponse } from "next/og";
 import { readFileSync } from "fs";
 import { join } from "path";
 
-export const size = { width: 180, height: 180 };
+export const size = { width: 32, height: 32 };
 export const contentType = "image/png";
 
-export default function AppleIcon() {
+export default function Icon() {
   const imgBuffer = readFileSync(join(process.cwd(), "src/app/setthetable_nobg.png"));
-  const base64 = imgBuffer.toString("base64");
-  const dataUrl = `data:image/png;base64,${base64}`;
+  const dataUrl = `data:image/png;base64,${imgBuffer.toString("base64")}`;
 
   return new ImageResponse(
     (
       <div
         style={{
-          width: 180,
-          height: 180,
-          borderRadius: 40,
-          backgroundColor: "#162D5A",
+          width: 32,
+          height: 32,
+          borderRadius: 7,
+          backgroundColor: "#1e3a6e",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={dataUrl} width={120} height={120} alt="" />
+        <img src={dataUrl} width={22} height={22} alt="" />
       </div>
     ),
     { ...size }
